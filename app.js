da81a4ebf6ecf371d2ba2852278e6e31dbf6b615
xiaoshen <dora@xiaoshendeMac-mini.local>
2020-10-03 12:03:59 +0800

    update egg-doras-validate

---
+++ app.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const Parameter = require('parameter');
+
+const moment = require('moment');
+module.exports = app => {
+  /**
+   * Validate
+   *
+   * ```js
+   * app.validator.addRule('jsonString', (rule, value) => {
+   *   try {
+   *     JSON.parse(value);
+   *   } catch (err) {
+   *     return 'must be json string';
+   *   }
+   * });
+   *
+   * app.validator.validate({
+   * 	 name: 'string',
+   * 	 info: { type: 'jsonString', required: false },
+   * }, {
+   *   name: 'Egg',
+   *   info: '{"foo": "bar"}',
+   * });
+   * ```
+   */
+  app.validator = new Parameter(app.config.validate);
+};
